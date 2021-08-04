fc27801c1d553ccb72f1f9a36f5eb585a1b1f92a
TTYTTDK <70526980+TTYTTDK@users.noreply.github.com>
2021-08-04 12:14:00 +0800

    update the interactive all rmse chart

--- js/rmsebar.js
+++ js/rmsebar.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-const margin = {top: 20, right: 10, bottom: 80, left: 50},
+const margin = {top: 10, right: 10, bottom: 80, left: 35},
       width = 1100 - margin.left - margin.right,
       height = 600 - margin.top - margin.bottom;
 
@@ -16,16 +16,17 @@ const svg = d3.select("#rmsebar")
 // Parse the Data
 d3.csv("data/All_Metrics_Score_rmse_flat_table.csv").then( function(data) {
 
+  console.log(data.columns.slice(1,3).concat(data.columns.slice(5,7)))
+
+
   // List of subgroups = header of the csv files = soil condition here
-  const subgroups = data.columns.slice(1)
+  let subgroups = data.columns.slice(1)
 
   // List of groups = species here = value of the first column called group -> I show them on the X axis
-  const groups = data.map(d => d.name)
-
-  console.log(groups)
+  let groups = data.map(d => d.name)
 
   // Add X axis
-  const x = d3.scaleBand()
+  let x = d3.scaleBand()
               .domain(groups)
               .range([0, width])
               .padding([0.15])
@@ -34,28 +35,30 @@ d3.csv("data/All_Metrics_Score_rmse_flat_table.csv").then( function(data) {
       .call(d3.axisBottom(x).tickSize(0))
       .selectAll("text")
       .style("font-size","14px")
+      .style("font-weight", "bold")
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
   // Add Y axis
-  const y = d3.scaleLinear()
-              .domain([0, 80])
-              .range([height, 0 ]);
+  let y = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d3.max(subgroups, key => +d[key]))]).nice()
+            .range([height, 0 ]);
   svg.append("g")
       .call(d3.axisLeft(y))
       .selectAll("text")
-      .style("font-size","14px");
+      .style("font-size","14px")
+      .style("font-weight", "bold");
 
   // Another scale for subgroup position?
-  const xSubgroup = d3.scaleBand()
-                        .domain(subgroups)
-                        .range([0, x.bandwidth()])
-                        .padding([0.03])
+  let xSubgroup = d3.scaleBand()
+                    .domain(subgroups)
+                    .range([0, x.bandwidth()])
+                    .padding([0.03])
 
   // color palette = one color per subgroup
-  const color = d3.scaleOrdinal()
-                    .domain(subgroups)
-                    .range(['#ea8c55','#cad2c5','#c75146','#84a98c', '#ad2e24','#52796f','#81171b','#354f52','#540804','#2f3e46'])
+  let color = d3.scaleOrdinal()
+                .domain(subgroups)
+                .range(['#ea8c55','#cad2c5','#c75146','#84a98c', '#ad2e24','#52796f','#81171b','#354f52','#540804','#2f3e46'])
 
 
   // Show the bars
